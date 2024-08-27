a5f2777cd5ac57f552a5cadd9f0a129800c53625
Shiva <kumar.shivasai8@gmail.com>
2024-08-27 13:34:42 -0400

    image carousel left arrow fix

--- src/Components/ImageCarousel.js
+++ src/Components/ImageCarousel.js
@@ -8,29 +8,6 @@ const Carousel = ({ images }) => {
   const [currentIndex, setCurrentIndex] = useState(0);
   const [direction, setDirection] = useState(null);
 
-  // const slideVariants = {
-  //   hiddenRight: {
-  //     x: "100%",
-  //     opacity: 1,
-  //   },
-  //   hiddenLeft: {
-  //     x: "-100%",
-  //     opacity: 1,
-  //   },
-  //   visible: {
-  //       x:"0",
-  //     opacity: 1,
-  //     transition: {
-  //       duration: 1,
-  //     },
-  //   },
-  //   exit: {
-  //     opacity: 0,
-  //     transition: {
-  //       duration: 1,
-  //     },
-  //   },
-  // };
   
   const sliderVariants = {
     incoming: direction => ({
